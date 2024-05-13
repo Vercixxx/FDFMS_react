bc3f904a7bba38bdf2aede8229c4878dd040d91e
Vercixxx <krz.sluzalek@gmail.com>
2024-05-13 18:58:14 +0200

    Added logo

--- src/components/MyAppBar.tsx
+++ src/components/MyAppBar.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
+// Logo
+import logo from "../assets/logo.png";
+
 // Icons
 import Icon from "@mdi/react";
 import { mdiThemeLightDark, mdiLogout } from "@mdi/js";
@@ -26,18 +29,16 @@ const MyAppBar = () => {
     //     className="mb-5"
     //     sx={{ bgcolor: "rgba(50,100 ,2,0.3)", color: "black" }}
     //   >
-    <div
-      className="grid grid-cols-12 gap-4 py-2 bg-green-500 text-white shadow-md"
-
-    >
+    <div className="grid grid-cols-12 gap-4 py-2 bg-green-500 text-white shadow-md">
       {/* 1 */}
-      <div className="col-span-1 flex justify-start items-center ps-4">
-        FDFMS
+      <div className="col-span-2 flex justify-start items-center ps-4 text-justify text-sm font-black">
+        <img src={logo} alt="logo" className="h-16 rounded-full" />
+        Food Delivery Fleet Management System
       </div>
       {/* 1 */}
 
       {/* 2 */}
-      <div className="col-span-10 flex justify-start items-center">
+      <div className="col-span-9 flex justify-start items-center">
         <MyBreadcrumb />
       </div>
       {/* 2 */}
@@ -45,7 +46,7 @@ const MyAppBar = () => {
       {/* 3 */}
       <div className="col-span-1 flex justify-end items-center pe-4">
         {/* Theme */}
-        <IconButton color="inherit"  className="hover:scale-110">
+        <IconButton color="inherit" className="hover:scale-110">
           <Icon path={mdiThemeLightDark} size={1} />
         </IconButton>
         {/* Theme */}
@@ -55,7 +56,11 @@ const MyAppBar = () => {
           openModal={openLogOutModal}
           setOpenModal={setOpenLogOutModal}
         />
-        <IconButton color="inherit" onClick={showLogOutModal} className="hover:scale-110">
+        <IconButton
+          color="inherit"
+          onClick={showLogOutModal}
+          className="hover:scale-110"
+        >
           <Icon path={mdiLogout} size={1} />
         </IconButton>
         {/* Logout */}
