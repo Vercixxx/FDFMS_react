3a6c27cf7d14c28c5a4b98d925d1a9c494e5af34
Vercixxx <krz.sluzalek@gmail.com>
2024-05-16 23:21:23 +0200

    Added adaptive UI for table in mobile devices. Added export to pdf and excel.

--- src/pages/Other/ManageCountries.tsx
+++ src/pages/Other/ManageCountries.tsx
@@ -9,9 +9,16 @@ import {
   ColumnDirective,
   ColumnsDirective,
   EditSettingsModel,
+  Grid,
   GridComponent,
+  Inject,
+  Page,
+  Edit,
+  Toolbar,
+  ToolbarItems,
+  ExcelExport,
+  PdfExport,
 } from "@syncfusion/ej2-react-grids";
-import { Inject, Page, Edit, Toolbar, ToolbarItems } from "@syncfusion/ej2-react-grids";
 
 // Theme
 import { ThemeContext } from "../../config/ThemeContext";
@@ -20,8 +27,6 @@ import { ThemeContext } from "../../config/ThemeContext";
 import "./ManageCountriesStyle.css";
 
 // Icons
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
 import FlagIcon from "@mui/icons-material/Flag";
 
 const ManageCountriesComponent = () => {
@@ -30,7 +35,6 @@ const ManageCountriesComponent = () => {
   React.useEffect(() => {
     const fetchData = async () => {
       const data = await GetCountries();
-      console.log(data);
       setCountries(data);
     };
 
@@ -46,10 +50,93 @@ const ManageCountriesComponent = () => {
   const pageSettings = { pageSize: 15 };
 
   // Syncfusion - Edit
-  const editOptions: EditSettingsModel = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: "Dialog"};
+  const editOptions: EditSettingsModel = {
+    allowEditing: true,
+    allowAdding: true,
+    allowDeleting: true,
+    mode: "Dialog",
+  };
+
+  // Syncfusion - Exporting
+  let grid: Grid | null;
+  const toolbarClick = (args: any) => {
+    if (grid) {
+      if (args.item.id.includes("excelexport")) {
+        grid.excelExport({
+          fileName: "Countries.xlsx",
+          header: {
+            headerRows: 1,
+            rows: [
+              {
+                cells: [
+                  {
+                    colSpan: 2,
+                    value: "Food Delivery Fleet Management System - Countries",
+                    style: {
+                      hAlign: "Center",
+                      bold: true,
+                      fontColor: "#C67878",
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+          footer: {
+            footerRows: 1,
+            rows: [
+              {
+                cells: [
+                  {
+                    colSpan: 2,
+                    value: `Generated ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`,
+                    style: { hAlign: "Center", bold: true },
+                  },
+                ],
+              },
+            ],
+          },
+        });
+      } else if (args.item.id.includes("pdfexport")) {
+        grid.pdfExport({
+          fileName: "Countries.pdf",
+          header: {
+            fromTop: 0,
+            height: 130,
+            contents: [
+              {
+                type: "Text",
+                value: "Food Delivery Fleet Management System - Countries",
+                position: { x: 0, y: 50 },
+                style: { textBrushColor: "#000000", fontSize: 14 },
+              },
+            ],
+          },
+          footer: {
+            fromBottom: 0,
+            height: 130,
+            contents: [
+              {
+                type: "Text",
+                value: `Generated ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`,
+                position: { x: 0, y: 0 },
+                style: { textBrushColor: "#000000", fontSize: 14 },
+              },
+            ],
+          },
+        });
+      }
+    }
+  };
 
   // Syncfusion - Toolbar
-  const toolbarOptions: ToolbarItems[] = ["Add", "Edit", "Delete"];
+  const toolbarOptions: ToolbarItems[] = [
+    "Add",
+    "Edit",
+    "Delete",
+    "ExcelExport",
+    "PdfExport",
+  ];
 
   // Syncfusion
 
@@ -61,17 +148,23 @@ const ManageCountriesComponent = () => {
       </div>
 
       <GridComponent
+        ref={(g) => (grid = g)}
         dataSource={countries}
         allowPaging={true}
         pageSettings={pageSettings}
         editSettings={editOptions}
         toolbar={toolbarOptions}
+        toolbarClick={toolbarClick}
+        allowExcelExport={true}
+        allowPdfExport={true}
+        enableAdaptiveUI={true}
+        rowRenderingMode="Vertical"
       >
         <ColumnsDirective>
-          <ColumnDirective field="Id" textAlign="Left" />
+          <ColumnDirective field="Id" textAlign="Left" allowEditing={false} />
           <ColumnDirective field="Name" textAlign="Left" />
         </ColumnsDirective>
-        <Inject services={[Page, Edit, Toolbar]} />
+        <Inject services={[Page, Edit, Toolbar, ExcelExport, PdfExport]} />
       </GridComponent>
     </div>
   );
