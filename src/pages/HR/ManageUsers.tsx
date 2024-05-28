ef0328c28493082455e4417c08654e725086e5cc
Vercixxx <krz.sluzalek@gmail.com>
2024-05-28 19:31:47 +0200

    Added display mode switch in table and fixed error in ManageUsers

--- src/pages/HR/ManageUsers.tsx
+++ src/pages/HR/ManageUsers.tsx
@@ -27,6 +27,8 @@ import ClearIcon from "@mui/icons-material/Clear";
 
 // Mui
 import Button from "@mui/material/Button";
+import Switch, { SwitchProps } from "@mui/material/Switch";
+import { alpha, styled } from "@mui/material/styles";
 
 // Syncfusion
 import {
@@ -58,11 +60,13 @@ import {
   PlusCircleFilled,
 } from "@ant-design/icons";
 import {
+  Stack,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 
 const ManageUsersComponent: React.FC = () => {
@@ -80,7 +84,7 @@ const ManageUsersComponent: React.FC = () => {
     previous: string | null;
     total_results: number | null;
     total_pages: number | null;
-    results: any[] | null;
+    results: any[];
   }
 
   // Get users
@@ -92,16 +96,17 @@ const ManageUsersComponent: React.FC = () => {
     total_pages: 0,
     results: [],
   });
+
+  const fetchData = async () => {
+    const data = await getUsers(RequestParams.current);
+    if (data.type === "error") {
+      console.log(data);
+      showSnackbar(data.message, "error");
+    } else {
+      setResponse(data);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getUsers(RequestParams.current);
-      if (data.type === "error") {
-        console.log(data);
-        showSnackbar(data.message, "error");
-      } else {
-        setResponse(data);
-      }
-    };
     fetchData();
   }, []);
 
@@ -260,7 +265,7 @@ const ManageUsersComponent: React.FC = () => {
 
   //   Sorting
   const sortOptions: SortSettingsModel = {
-    columns: [{ field: "Name", direction: "Ascending" }],
+    columns: [{ field: "Username", direction: "Ascending" }],
   };
 
   //   Details
@@ -292,6 +297,229 @@ const ManageUsersComponent: React.FC = () => {
     );
   };
 
+  // Details mode
+  const [manipulateMode, setDetailsMode] = useState(false);
+  const switchDetailsMode = (event, checked) => {
+    setDetailsMode(checked);
+  };
+  const PinkSwitch = styled(Switch)(({ theme }) => ({
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      color: "#0f0",
+    },
+    "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+      backgroundColor: "#0f0",
+    },
+    "&:hover": {
+      backgroundColor: alpha("#cf2", theme.palette.action.hoverOpacity),
+    },
+  }));
+  // Syncfusion - Custom toolbar
+  const toolbarTemplate = () => {
+    return (
+      <div className="flex justify-between mb-4">
+        <div className="flex justify-start space-x-4">
+          <Button
+            variant="outlined"
+            startIcon={<PlusCircleFilled />}
+            className="hover:scale-105"
+            sx={{
+              borderRadius: "12px",
+            }}
+            color="success"
+            onClick={() => {
+              dispatch(
+                openDrawer({
+                  title: t("Add State"),
+                  component: (
+                    // <DialogStateTemplate
+                    //   refreshComponent={refreshComponent}
+                    //   setRefreshComponent={setRefreshComponent}
+                    // />
+                    <></>
+                  ),
+                })
+              );
+            }}
+          >
+            {t("Add")}
+          </Button>
+
+          <Button
+            variant="outlined"
+            startIcon={<EditFilled />}
+            disabled={selectedRows.length === 0 || selectedRows.length > 1}
+            className="hover:scale-105"
+            sx={{
+              borderRadius: "12px",
+            }}
+            color="success"
+            onClick={() => {
+              const selectedRecord = grid.current?.getSelectedRecords();
+              if (selectedRecord && selectedRecord.length == 1) {
+                dispatch(
+                  openDrawer({
+                    title: t("Edit State"),
+                    component: (
+                      // <DialogStateTemplate
+                      //   refreshComponent={refreshComponent}
+                      //   setRefreshComponent={setRefreshComponent}
+                      //   data={selectedRecord[0]}
+                      // />
+                      <></>
+                    ),
+                  })
+                );
+              }
+            }}
+          >
+            {t("Edit")}
+          </Button>
+
+          <Button
+            variant="outlined"
+            startIcon={<DeleteFilled />}
+            disabled={selectedRows.length === 0}
+            className="hover:scale-105"
+            sx={{
+              borderRadius: "12px",
+            }}
+            color="warning"
+            onClick={() => {
+              const selectedRecords = grid.current?.getSelectedRecords();
+              if (selectedRecords && selectedRecords.length > 0) {
+                showDeleteConfirm(selectedRecords[0]);
+              }
+            }}
+          >
+            {t("Delete")}
+
+            {selectedRows.length >= 1 ? ` ${selectedRows.length} ` : ""}
+            {selectedRows.length === 1 ? `${t("Item")}` : ""}
+            {selectedRows.length > 1 ? `${t("Items")}` : ""}
+          </Button>
+        </div>
+
+        <div className="flex justify-end space-x-4">
+          <Button
+            variant="outlined"
+            startIcon={<FileExcelFilled />}
+            className="hover:scale-105"
+            sx={{
+              borderRadius: "12px",
+            }}
+            color="info"
+            disabled={
+              exportType === "SelectedRows" &&
+              grid.current?.getSelectedRecords().length === 0
+            }
+            onClick={() => {
+              if (
+                exportType === "SelectedRows" &&
+                grid.current?.getSelectedRecords().length === 0
+              ) {
+                showSnackbar(t("Please select at least one row"), "error");
+              } else {
+                grid.current?.excelExport({
+                  dataSource: ExportData(exportType),
+                  fileName: `${t("States")}.xlsx`,
+                  header: {
+                    headerRows: 1,
+                    rows: [
+                      {
+                        cells: [
+                          {
+                            colSpan: 2,
+                            value: `Food Delivery Fleet Management System - ${t(
+                              "States"
+                            )}`,
+                            style: {
+                              hAlign: "Center",
+                              bold: true,
+                              fontColor: "#C67878",
+                            },
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                  footer: {
+                    footerRows: 1,
+                    rows: [
+                      {
+                        cells: [
+                          {
+                            colSpan: 2,
+                            value: `Generated ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`,
+                            style: { hAlign: "Center", bold: true },
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                });
+              }
+            }}
+          >
+            {t("Export to Excel")}
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<FilePdfFilled />}
+            className="hover:scale-105"
+            sx={{
+              borderRadius: "12px",
+            }}
+            color="info"
+            disabled={
+              exportType === "SelectedRows" &&
+              grid.current?.getSelectedRecords().length === 0
+            }
+            onClick={() => {
+              if (
+                exportType === "SelectedRows" &&
+                grid.current?.getSelectedRecords().length === 0
+              ) {
+                showSnackbar(t("Please select at least one row"), "error");
+              } else {
+                grid.current?.pdfExport({
+                  dataSource: ExportData(exportType),
+                  fileName: `${t("States")}.pdf`,
+                  header: {
+                    fromTop: 0,
+                    height: 130,
+                    contents: [
+                      {
+                        type: "Text",
+                        value: `Food Delivery Fleet Management System - ${t(
+                          "States"
+                        )}`,
+                        position: { x: 0, y: 50 },
+                        style: { textBrushColor: "#000000", fontSize: 14 },
+                      },
+                    ],
+                  },
+                  footer: {
+                    fromBottom: 0,
+                    height: 130,
+                    contents: [
+                      {
+                        type: "Text",
+                        value: `Generated ${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}`,
+                        position: { x: 0, y: 0 },
+                        style: { textBrushColor: "#000000", fontSize: 14 },
+                      },
+                    ],
+                  },
+                });
+              }
+            }}
+          >
+            {t("Export to PDF")}
+          </Button>
+        </div>
+      </div>
+    );
+  };
   // =================== Grid ===================
 
   const openSettingsDrawer = () => {
@@ -310,6 +538,15 @@ const ManageUsersComponent: React.FC = () => {
         ),
       })
     );
+  };
+
+  // Details mode
+  const DetailsMode = () => {
+    return <div></div>;
+  };
+
+  const ManipulateMode = () => {
+    return <div>{toolbarTemplate()}</div>;
   };
 
   return (
@@ -331,14 +568,32 @@ const ManageUsersComponent: React.FC = () => {
         </div>
       </div>
 
+      <div className="border-2 ps-2 pt-2 pb-2 mb-3 max-w-56">
+        <div className="text-2xl font-black text-center">Display mode</div>
+
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Typography>
+            <span className={manipulateMode ? "" : "font-black"}>Details</span>
+          </Typography>
+          <PinkSwitch checked={manipulateMode} onChange={switchDetailsMode} />
+          <Typography>
+            <span className={manipulateMode ? "font-black" : ""}>
+              Manipulate
+            </span>
+          </Typography>
+        </Stack>
+      </div>
+
+      {manipulateMode ? <ManipulateMode /> : <DetailsMode />}
+
       <GridComponent
         ref={grid as React.RefObject<GridComponent>}
         allowSelection={true}
         queryCellInfo={customCell}
         selectionSettings={selectionOptions}
-        dataSource={response?.results || []}
-        allowPaging={true}
-        pageSettings={pageSettings}
+        dataSource={response.results}
+        // allowPaging={true}
+        // pageSettings={pageSettings}
         allowExcelExport={true}
         allowPdfExport={true}
         // enableAdaptiveUI={true}
@@ -356,9 +611,13 @@ const ManageUsersComponent: React.FC = () => {
         rowDeselected={(args) => {
           setSelectedRows(grid.current?.getSelectedRecords());
         }}
-        detailTemplate={detailsTemplate}
+        detailTemplate={manipulateMode ? undefined : detailsTemplate}
+        // detailTemplate={detailsTemplate}
       >
         <ColumnsDirective>
+          {manipulateMode ? (
+            <ColumnDirective type="checkbox" width="50" />
+          ) : null}
           {columns.map((column) => (
             <ColumnDirective
               field={column.field}
